e3547edc646258bcd743cf071fb77b7122bbf86a
Nhan BIT <nhan.cao@bitelle.com>
2024-02-06 14:55:59 +0700

    Config connect to backend host

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { PathnameContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 import { NextRequest } from "next/server";
 import { NextResponse as res } from "next/server";
 
@@ -10,7 +9,7 @@ export default async function middleware(req: NextRequest) {
   const pathName = req.nextUrl.pathname;
 
   if (pathName.startsWith("/api")) {
-    const url = `${process.env.DETECT_TEXT_API}${PathnameContext}`;
+    const url = `${process.env.DETECT_TEXT_API}${pathName}`;
     return res.rewrite(url);
   }
 
